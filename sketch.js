e5c0caabb3668562c3a1bc85dccd33947d517ae1
thismeiko <114015040+thismeiko@users.noreply.github.com>
2022-10-25 00:07:15 -0500

    1

--- sketch.js
+++ sketch.js
@@ -1,116 +1,97 @@
 let faceapi;
-let detections=[];
+let detections = [];
 
-let Canvas;
 let video;
+let canvas;
 
 function setup() {
- Canvas = createCanvas(480, 360);
-  Canvas.id('canvas');
+  canvas = createCanvas(480, 360);
+  canvas.id("canvas");
 
-  // load up your video
-  video = createCapture(VIDEO);
+  video = createCapture(VIDEO);// Creat the video: ビデオオブジェクトを作る
+  video.id("video");
   video.size(width, height);
-  video.id('video');
 
-  // by default all options are set to true
-const faceOptions = {
+  const faceOptions = {
     withLandmarks: true,
     withExpressions: true,
-    withDescriptors: false,
-    minCofidence:0.5
+    withDescriptors: true,
+    minConfidence: 0.5
   };
 
-  // video.hide(); // Hide the video element, and just show the canvas
+  //Initialize the model: モデルの初期化
   faceapi = ml5.faceApi(video, faceOptions, faceReady);
-  textAlign(RIGHT);
 }
 
 function faceReady() {
-  console.log("ready!");
-  console.log(faceapi);
-  faceapi.detect(gotFaces);
+  faceapi.detect(gotFaces);// Start detecting faces: 顔認識開始
 }
 
+// Got faces: 顔を検知
 function gotFaces(error, result) {
   if (error) {
     console.log(error);
     return;
   }
 
-  // console.log(result)
-  detections = result;
+  detections = result;　//Now all the data in this detections: 全ての検知されたデータがこのdetectionの中に
+  // console.log(detections);
 
-  clear();
-drawBoxes(detections);
-drawLandmarks(detections);
-drawExpressions(detections,20,250,14);
+  clear();//Draw transparent background;: 透明の背景を描く
+  drawBoxs(detections);//Draw detection box: 顔の周りの四角の描画
+  drawLandmarks(detections);//// Draw all the face points: 全ての顔のポイントの描画
+  drawExpressions(detections, 20, 250, 14);//Draw face expression: 表情の描画
 
-console.log(detections);
-faceapi.detect(gotFaces);
+  faceapi.detect(gotFaces);// Call the function again at here: 認識実行の関数をここでまた呼び出す
 }
 
-function draw(){
-
+function drawBoxs(detections){
+  if (detections.length > 0) {//If at least 1 face is detected: もし1つ以上の顔が検知されていたら
+    for (f=0; f < detections.length; f++){
+      let {_x, _y, _width, _height} = detections[f].alignedRect._box;
+      stroke(44, 169, 225);
+      strokeWeight(1);
+      noFill();
+      rect(_x, _y, _width, _height);
+    }
+  }
 }
-  
-function drawBoxes(detections){
-  if (detections) {
-    if (detections.length > 0) {
-      // console.log(detections)
-      for(var f=0;f<detections.length; f++){
-        // let x = detections[0].alignedRect._box._x;
-        // let y = detections[0].alignedRect._box._y;
-        // let rectWidth = detections[0].alignedRect._box._width;
-        // let rectHeight = detections[0].alignedRect._box._height;
-        let {_x,_y,_width,_height} = detections[0].alignedRect._box;
 
-        stroke(44,169,225);
-        strokeWeight(1);
-        noFill();
-        rect(_x,_y,_width,_height);
-
+function drawLandmarks(detections){
+  if (detections.length > 0) {//If at least 1 face is detected: もし1つ以上の顔が検知されていたら
+    for (f=0; f < detections.length; f++){
+      let points = detections[f].landmarks.positions;
+      for (let i = 0; i < points.length; i++) {
+        stroke(44, 169, 225);
+        strokeWeight(3);
+        point(points[i]._x, points[i]._y);
       }
     }
   }
 }
 
-  function drawLandmarks(detections){
-    if(detections){
-        if(detections.length>0){
-            for(f=0; f<detections.length; f++){
-                let points =detections[f].landmarks.positions;
-                for(let i=0; i<points.length; i++){
-                    stroke(44,169,255);
-                    strokeWeight(3);
-                    point(points[i]._x,points[i]._y);
-                }
-            }
-        }
-        }
-    }
+function drawExpressions(detections, x, y, textYSpace){
+  if(detections.length > 0){//If at least 1 face is detected: もし1つ以上の顔が検知されていたら
+    let {neutral, happy, angry, sad, disgusted, surprised, fearful} = detections[0].expressions;
+    textFont('Helvetica Neue');
+    textSize(14);
+    noStroke();
+    fill(44, 169, 225);
 
-    function drawExpressions(detections, x, y, textYspace){
-        textFont('Helvetica Neue');
-        textSize(14);
-        noStroke();
-        fill(44,169,225);
-
-        if(detections.length>0){
-            let {neutral, happy, angry, sad, disgusted, surprised, fearful} 
-            = detections[0].expressions;
-
-            text("neutral:" + nf(neutral *100,2,2) + "%",x,y);
-            text("happy:" + nf(neutral *100,2,2) + "%",x,y + textYspace);
-            text("sad:" + nf(neutral *100,2,2) + "%",x,y + textYspace*2);
-        }
-        else{
-            text("neutral:" , x,y);
-            text("happy:" ,x,y + textYspace);
-            text("sad:" ,x,y + textYspace*2);
-        }
-    }
-  
-
-
-
+    text("neutral:       " + nf(neutral*100, 2, 2)+"%", x, y);
+    text("happiness: " + nf(happy*100, 2, 2)+"%", x, y+textYSpace);
+    text("anger:        " + nf(angry*100, 2, 2)+"%", x, y+textYSpace*2);
+    text("sad:            "+ nf(sad*100, 2, 2)+"%", x, y+textYSpace*3);
+    text("disgusted: " + nf(disgusted*100, 2, 2)+"%", x, y+textYSpace*4);
+    text("surprised:  " + nf(surprised*100, 2, 2)+"%", x, y+textYSpace*5);
+    text("fear:           " + nf(fearful*100, 2, 2)+"%", x, y+textYSpace*6);
+  }else{//If no faces is detected: 顔が1つも検知されていなかったら
+    text("neutral: ", x, y);
+    text("happiness: ", x, y + textYSpace);
+    text("anger: ", x, y + textYSpace*2);
+    text("sad: ", x, y + textYSpace*3);
+    text("disgusted: ", x, y + textYSpace*4);
+    text("surprised: ", x, y + textYSpace*5);
+    text("fear: ", x, y + textYSpace*6);
+  }
+}
