fc514c7b21f5e28c3a6a7ec85a79438f59189c25
thismeiko <114015040+thismeiko@users.noreply.github.com>
2022-10-25 13:44:34 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -34,13 +34,12 @@ function gotFaces(error, result) {
     return;
   }
 
-  detections = result;　//Now all the data in this detections: 全ての検知されたデータがこのdetectionの中に
+  detections = result;//Now all the data in this detections: 全ての検知されたデータがこのdetectionの中に
   // console.log(detections);
 
   clear();//Draw transparent background;: 透明の背景を描く
   drawBoxs(detections);//Draw detection box: 顔の周りの四角の描画
   drawLandmarks(detections);//// Draw all the face points: 全ての顔のポイントの描画
-  drawExpressions(detections, 20, 250, 14);//Draw face expression: 表情の描画
 
   faceapi.detect(gotFaces);// Call the function again at here: 認識実行の関数をここでまた呼び出す
 }
@@ -70,28 +69,3 @@ function drawLandmarks(detections){
   }
 }
 
-function drawExpressions(detections, x, y, textYSpace){
-  if(detections.length > 0){//If at least 1 face is detected: もし1つ以上の顔が検知されていたら
-    let {neutral, happy, angry, sad, disgusted, surprised, fearful} = detections[0].expressions;
-    textFont('Helvetica Neue');
-    textSize(14);
-    noStroke();
-    fill(44, 169, 225);
-
-    text("neutral:       " + nf(neutral*100, 2, 2)+"%", x, y);
-    text("happiness: " + nf(happy*100, 2, 2)+"%", x, y+textYSpace);
-    text("anger:        " + nf(angry*100, 2, 2)+"%", x, y+textYSpace*2);
-    text("sad:            "+ nf(sad*100, 2, 2)+"%", x, y+textYSpace*3);
-    text("disgusted: " + nf(disgusted*100, 2, 2)+"%", x, y+textYSpace*4);
-    text("surprised:  " + nf(surprised*100, 2, 2)+"%", x, y+textYSpace*5);
-    text("fear:           " + nf(fearful*100, 2, 2)+"%", x, y+textYSpace*6);
-  }else{//If no faces is detected: 顔が1つも検知されていなかったら
-    text("neutral: ", x, y);
-    text("happiness: ", x, y + textYSpace);
-    text("anger: ", x, y + textYSpace*2);
-    text("sad: ", x, y + textYSpace*3);
-    text("disgusted: ", x, y + textYSpace*4);
-    text("surprised: ", x, y + textYSpace*5);
-    text("fear: ", x, y + textYSpace*6);
-  }
-}
